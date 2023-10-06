57900829fb3046b2671e7881619b71fdc0250171
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-10-06 21:11:32 +0900

    🎨 アプリからも色々修正できるようにするため頭の「m_」を消す

--- models.py
+++ models.py
@@ -10,26 +10,26 @@ class WebPage(Base):
     description = Column(String)
     image = Column(String, nullable=True)
     url = Column(String)
-    main_property_id = Column(Integer, ForeignKey('m_main_properties.id'))
+    main_property_id = Column(Integer, ForeignKey('main_properties.id'))
     sub_property_list = relationship('WebPageSubProperty', backref='web_pages')
     
 class WebPageSubProperty(Base):
     __tablename__ = 'web_page_sub_properties'
     id = Column(Integer, primary_key=True)
     web_page_id = Column(Integer, ForeignKey('web_pages.id'))
-    sub_property_id = Column(Integer, ForeignKey('m_sub_properties.id'))
+    sub_property_id = Column(Integer, ForeignKey('sub_properties.id'))
     
 
 class MainProperty(Base):
-    __tablename__ = 'm_main_properties'
+    __tablename__ = 'main_properties'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    web_pages = relationship('WebPage', backref='m_main_properties')
-    sub_property_list = relationship('SubProperty', backref='m_main_properties')
+    web_pages = relationship('WebPage', backref='main_properties')
+    sub_property_list = relationship('SubProperty', backref='main_properties')
     
 class SubProperty(Base):
-    __tablename__ = 'm_sub_properties'
+    __tablename__ = 'sub_properties'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    main_property_id = Column(Integer, ForeignKey('m_main_properties.id'))
+    main_property_id = Column(Integer, ForeignKey('main_properties.id'))
     
